71e479edea7427c4e275af642b997ccb168c6000
Moonwalker1996 <54628577+Moonwalker1996@users.noreply.github.com>
2020-04-03 16:31:17 +0400

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ async def coin(ctx, args):
 
 @client.command()
 async def helping(ctx, args):
-	if args == None:
+	if args == 'server':
 		await ctx.send("Для информации насчет ботов перейдите в #Information.Для получения информации о каком-то канале,после комнады напиши назвние канала.")
 	elif args == 'chat':
 		await ctx.send("Этот канал для общения и разговоров")
