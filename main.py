df25de50097dc4e4a5882ec970bae865973d8d5a
Moonwalker1996 <54628577+Moonwalker1996@users.noreply.github.com>
2020-04-08 02:13:40 +0400

    Update main.py

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ async def w(ctx):
 async def s(ctx):
     for guild in client.guilds:
         for member in guild.members:
-            await ctx.send(len(list(member)))
+            await ctx.send(member)
 
 # RUN
 token = os.environ.get('BOT_TOKEN')
