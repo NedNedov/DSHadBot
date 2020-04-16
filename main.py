fbe797dbf87eff610ca69df33e24b159437319b1
Moonwalker1996 <54628577+Moonwalker1996@users.noreply.github.com>
2020-04-16 21:02:17 +0400

    Update main.py

--- main.py
+++ main.py
@@ -82,7 +82,8 @@ async def cr(ctx):
 # member online
 @client.command()
 async def mon(ctx):
-    await ctx.send("Members online: " + str(len(ctx.guild.members)))
+    await ctx.send("All members: " + str(len(ctx.guild.members)))
+    await ctx.send("Online: " + str(len(ctx.guild.members.online)))
 
 # random num bot
 @client.command()
